1e6051429b99e60836af929ce429e24faa45277c
Imane <imaneelmouradi98@gmail.com>
2019-05-03 01:05:42 +0100

    added api now working on display(css)

---
+++ src/components/Main.jsx
@@ -0,0 +1,40 @@
+//https://www.food2fork.com/about/api
+
+import React, { Component } from "react";
+
+import Form from "./Form";
+import Recipes from "./Recipes";
+
+const API_KEY = "1dcbf73b1f90d1c40167264485eede61";
+
+class Main extends Component {
+  state = {
+    recipes: []
+  };
+
+  getRecipe = async e => {
+    const recipeName = e.target.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(
+      `https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`
+    );
+
+    const data = await api_call.json();
+
+    this.setState({ recipes: data.recipes });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">recipe search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
+      </div>
+    );
+  }
+}
+
+export default Main;
